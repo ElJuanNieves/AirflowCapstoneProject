c5ba2b690dab22a5fc5101c5413c8ca91688de8a
ElJuanNieves <rosorno307@gmail.com>
2025-06-30 12:35:47 -0600

    Add project files

--- dags/jobs_dag.py
+++ dags/jobs_dag.py
@@ -1,4 +1,10 @@
+"""
+DAG: jobs_dag.py
+Purpose:
+- jobs_dag: Simulates a data pipeline with tasks to print DB info, conditionally insert/query/create table, and return a result.
+"""
 from datetime import datetime
+
 from airflow import DAG
 from airflow.providers.standard.operators.bash import BashOperator
 from airflow.providers.standard.operators.empty import EmptyOperator
@@ -8,72 +14,55 @@ from airflow.utils.trigger_rule import TriggerRule
 config = {
     'dag_id_1': {'schedule_interval': None, "start_date": datetime(2018, 11, 11), 'database': 'db1'},
     'dag_id_2': {'schedule_interval': None, "start_date": datetime(2018, 11, 11), 'database': 'db2'},
-    'dag_id_3':{'schedule_interval': None, "start_date": datetime(2018, 11, 11), 'database': 'db3'},}
-
-dags = {}
+    'dag_id_3': {'schedule_interval': None, "start_date": datetime(2018, 11, 11), 'database': 'db3'}
+}
 
 def print_info(dag_id, database, **kwargs):
     print(f"{dag_id} start processing tables in database: {database}")
 
 def check_table_exist():
-    if True:
-        return 'insert_new_row'
-    return 'create_table'
+    return 'insert_new_row' if True else 'create_table'
 
+def push_message(**kwargs):
+    return f"{kwargs['dag_id']} run_id={{ run_id }} ended"
 
 for dag_id, params in config.items():
-    dag = DAG(
+    with DAG(
         dag_id,
-        start_date = params["start_date"],
-        schedule = params["schedule_interval"],
-        catchup = False
-    )
-    # Tarea 1: imprimir información
-    printInfo = PythonOperator(
-        task_id='print_info',
-        python_callable=print_info,
-        op_kwargs={'dag_id': dag_id, 'database': params['database']},
-        dag=dag
-    )
+        start_date=params["start_date"],
+        schedule=params["schedule_interval"],
+        catchup=False,
+    ) as dag:
 
-    # Tarea 2: mock de inserción
-    insert = EmptyOperator(
-        task_id='insert_new_row',
-        dag=dag,
-        trigger_rule = 'none_failed'
-    )
+        print_info_task = PythonOperator(
+            task_id='print_info',
+            python_callable=print_info,
+            op_kwargs={'dag_id': dag_id, 'database': params['database']}
+        )
 
-    # Tarea 3: mock de consulta
-    query = EmptyOperator(
-        task_id='query_the_table',
-        dag=dag
-    )
+        get_user = BashOperator(
+            task_id='get_current_user',
+            bash_command='whoami'
+        )
 
-    check = BranchPythonOperator(
-        task_id='check_table',
-        python_callable=check_table_exist,
-        dag=dag
-    )
+        check = BranchPythonOperator(
+            task_id='check_table',
+            python_callable=check_table_exist
+        )
 
-    create = EmptyOperator(
-        task_id='create_table',
-        dag=dag
-    )
+        create = EmptyOperator(task_id='create_table')
+        insert = EmptyOperator(task_id='insert_new_row', trigger_rule=TriggerRule.NONE_FAILED)
 
-    getUser = BashOperator(
-        task_id='get_current_user',
-        bash_command='whoami',
-        dag=dag
-    )
+        query = PythonOperator(
+            task_id='query_the_table',
+            python_callable=push_message,
+            op_kwargs={'dag_id': dag_id},
+            do_xcom_push=True
+        )
 
-    # Establecer dependencias
-    printInfo >> getUser >> check
-    check >> [create, insert]
-    create >> insert
-    insert >> query
-
-
+        print_info_task >> get_user >> check
+        check >> [create, insert]
+        create >> insert
+        insert >> query
 
     globals()[dag_id] = dag
-
-
